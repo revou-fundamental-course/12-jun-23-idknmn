6f4464182c320126594e1a32feaa660d636fc21b
idknmn <viryawushu@gmail.com>
2023-06-19 21:19:13 +0700

    Adding_Input

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,9 @@
 alert('Hello World');
+var input;
+  function myFunction() {
+    alert("fdsa");
+  }
+  function getValue() {
+    input = document.getElementById("Satuan_Suhu").value;
+    alert(input);
+  }
