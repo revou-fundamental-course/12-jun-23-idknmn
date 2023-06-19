38e859617216ca1e00e54acab3901bc34e0df370
idknmn <viryawushu@gmail.com>
2023-06-19 21:57:25 +0700

    Input placeholder sync

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,16 @@
 alert('Hello World');
+var button;
 var input;
+var Change_Name_placeholder;
+
   function myFunction() {
     alert("fdsa");
   }
   function getValue() {
-    input = document.getElementById("Satuan_Suhu").value;
+    var input = document.getElementById("Satuan_Suhu").value;
     alert(input);
+  }
+  function changeButtonPlaceholderName(value) {
+    var input = document.getElementById("Satuan_Suhu");
+    input.placeholder = value;
   }
